2affcb172f3ae417a1fe42d103712db854a8fc5d
Teodor Grigor <open-source@teogor.dev>
2023-09-08 17:34:37 +0300

    s

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,6 +1,30 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import dev.teogor.ceres.gradle.plugins.CeresLibraryExtension
+
 plugins {
   id("com.vanniktech.maven.publish")
   id("java-platform")
 }
 
 collectBomConstraints()
+
+afterEvaluate {
+  val ceresLibrary = CeresLibraryExtension().apply {
+    artifactId = "bom"
+    version = "2023.09.08"
+  }
+  mavenPublishing {
+    publishToMavenCentral(SonatypeHost.S01)
+    signAllPublications()
+
+    @Suppress("UnstableApiUsage")
+    pom {
+      coordinates(
+        groupId = ceresLibrary.groupId,
+        artifactId = ceresLibrary.artifactId!!,
+        version = ceresLibrary.version!!,
+      )
+      ceresLibrary.applyToMaven(this)
+    }
+  }
+}
