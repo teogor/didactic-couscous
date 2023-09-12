86f6dab910d8952d19ed592d43d4dc0235da1cf4
Teodor Grigor <open-source@teogor.dev>
2023-09-12 19:45:45 +0300

    Update MainActivity.kt

--- app/src/main/kotlin/dev/teogor/ceres/MainActivity.kt
+++ app/src/main/kotlin/dev/teogor/ceres/MainActivity.kt
@@ -25,16 +25,38 @@ import dev.teogor.ceres.menu.applyMenuConfig
 import dev.teogor.ceres.navigation.ApplyNavHost
 import dev.teogor.ceres.navigation.core.menu.TopLevelDestination
 
+/**
+ * The main activity of the Ceres application.
+ *
+ * This activity serves as the entry point for the application and handles navigation and menu configuration.
+ */
 @AndroidEntryPoint
 class MainActivity : Activity() {
 
-  // todo based on the screen we may have different top level destinations
+  /**
+   * Gets the list of top-level destinations based on the current screen.
+   *
+   * @return A list of [TopLevelDestination] for the current screen.
+   */
   override val topLevelDestinations: List<TopLevelDestination>
     get() = super.topLevelDestinations
 
+  /**
+   * Builds and applies the navigation graph for the Ceres application using [NavGraphOptions].
+   *
+   * This function is responsible for constructing and applying the navigation graph.
+   * It returns a [NavHost] composable representing the navigation graph.
+   *
+   * @return The [NavHost] composable representing the applied navigation graph.
+   */
   @Composable
   override fun NavGraphOptions.BuildNavGraph() = ApplyNavHost()
 
+  /**
+   * Builds the menu for the Ceres application using [MenuConfig].
+   *
+   * @return The configured [MenuConfig] instance.
+   */
   @Composable
   override fun MenuConfig.buildMenu() = applyMenuConfig()
 }
