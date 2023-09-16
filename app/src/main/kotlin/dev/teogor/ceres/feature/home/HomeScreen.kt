9e253d36fcf62f49a7e4de4fb834c546b895dae5
Teodor Grigor <open-source@teogor.dev>
2023-09-17 00:30:14 +0300

    Update HomeScreen.kt

--- app/src/main/kotlin/dev/teogor/ceres/feature/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/teogor/ceres/feature/home/HomeScreen.kt
@@ -29,6 +29,7 @@ import dev.teogor.ceres.framework.core.screen.showSettingsButton
 import dev.teogor.ceres.framework.core.screen.toolbarTitle
 import dev.teogor.ceres.framework.core.screen.toolbarTokens
 import dev.teogor.ceres.monetisation.messaging.ConsentManager
+import dev.teogor.ceres.monetisation.messaging.ConsentResult
 import dev.teogor.ceres.screen.builder.advancedView
 import dev.teogor.ceres.screen.builder.compose.ColumnLayout
 import dev.teogor.ceres.screen.builder.header
@@ -40,6 +41,15 @@ import dev.teogor.ceres.ui.theme.tokens.ColorSchemeKeyTokens
 internal fun HomeRoute(
   baseActions: BaseActions,
 ) {
+  val state by remember { ConsentManager.state }
+  val canRequestAds: Boolean = remember(state) {
+    when (val consentResult = state) {
+      is ConsentResult.ConsentFormAcquired -> consentResult.canRequestAds
+      is ConsentResult.ConsentFormDismissed -> consentResult.canRequestAds
+      else -> false
+    }
+  }
+
   baseActions.setScreenInfo {
     showNavBar {
       true
@@ -70,11 +80,11 @@ internal fun HomeRoute(
     }
   }
 
-
-  val state by remember { ConsentManager.state }
-  println("loadAndShowConsentFormIfRequired .. $state")
-
-  HomeScreen()
+  if (canRequestAds) {
+    HomeScreen()
+  } else {
+    ConsentManager.loadAndShowConsentFormIfRequired()
+  }
 }
 
 @Composable
